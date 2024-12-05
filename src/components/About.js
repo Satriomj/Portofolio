2252ff2eb934f3435815bb7a2fdcb50d02edb01f
i'm Yoo <94436311+Satriomj@users.noreply.github.com>
2024-12-05 14:23:00 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ const About = () => {
         <div className="w-full md:w-1/2">
           <h2 className="text-2xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">About Me</h2>
           <h1 className="text-3xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
-            Hello, I'm Frontend Developer
+            Hello, I'm Website Developer
           </h1>
           <p className="mb-4 leading-relaxed">
             Informatics Engineering graduate with an interest in website development as a Frontend Web Developer. Has internship experience at PT Darmajaya Digital Solusi and has two years of experience as a Frontend Web Developer. As well as leadership and active experience contributions to various organizations. Coworkers who are friendly and eager to learn new things.
